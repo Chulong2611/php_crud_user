7fee3c39f41a6c7a043839d2f4ecc183c262a3d4
Chu Long <longchu8922@gmail.com>
2025-09-25 14:29:49 +0700

    chinh sua de them user

--- models/UserModel.php
+++ models/UserModel.php
@@ -65,8 +65,8 @@ class UserModel extends BaseModel {
      * @return mixed
      */
     public function insertUser($input) {
-        $sql = "INSERT INTO `app_web1`.`users` (`name`, `password`) VALUES (" .
-                "'" . $input['name'] . "', '".md5($input['password'])."')";
+        $sql = "INSERT INTO `app_web1`.`users` (`name`, `password`, `fullname`, `email`, `type`) VALUES (" .
+                "'" . $input['name'] . "', '".md5($input['password'])."', '', '', '')";
 
         $user = $this->insert($sql);
 
